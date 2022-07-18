5deeb320585d56ce9dbc7f66d6de273108387fad
artem-mikhaylets <artem.mikhaylets@gmail.com>
2022-07-18 20:17:06 +0300

    last edit gulp and last edit first section

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,63 @@
-import gulp from 'gulp';
-import del from 'del';
-import minify from 'gulp-minify';
-import concat from 'gulp-concat';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
+// Основной модуль
+import gulp from "gulp";
+// Импорт путей
+import { path } from "./gulp/config/path.js";
+// Импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
 
-const sass = gulpSass(dartSass);
+// Передаем значения в глобальную переменную
+global.app = {
+	isBuild: process.argv.includes("--build"),
+	isDev: !process.argv.includes("--build"),
+	path: path,
+	gulp: gulp,
+	plugins: plugins,
+};
 
-gulp.task('styles', () => {
-    return gulp.src('./src/scss/**/*.scss', { sourcemaps: true })
-        .pipe(sass().on('error', sass.logError))
-		.pipe(minify({noSource: true}))
-		.pipe(concat('style.min.css'))
-        .pipe(gulp.dest('./dist/css/'));
-});
+// Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+// import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSpriteTask } from "./gulp/tasks/svg-sprive.js";
+import { zip } from "./gulp/tasks/zip.js";
+import { ftp } from "./gulp/tasks/ftp.js";
 
-gulp.task('scripts', () => {
-    return gulp.src('./src/js/**/*.js')
-		.pipe(minify({noSource: true}))
-		.pipe(concat('app.min.js'))
-        .pipe(gulp.dest('./dist/js/'));
-});
+// Наблюдатель за изменениями в файлах
+function watcher() {
+	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
+	gulp.watch(path.watch.scss, scss);
+	gulp.watch(path.watch.js, js);
+	gulp.watch(path.watch.images, images);
+}
 
-gulp.task('clean', () => {
-    return del([
-        './dest/css/style.min.css',
-		'./dest/js/app.min.js'
-    ]);
-});
+// Последовательная обработака шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
-gulp.task('watch', () => {
-    gulp.watch(['./src/**/*.scss', './src/js/**/*.js'], (done) => {
-        gulp.series(['clean', 'styles', 'scripts'])(done);
-    });
-});
+// Основные задачи
+const mainTasks = gulp.series(
+	fonts,
+	gulp.parallel(copy, html, scss, js, images, svgSpriteTask)
+);
 
-gulp.task('default', gulp.series(['clean', 'styles', 'scripts']));
+// Построение сценариев выполнения задач
+// const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher));
+const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
+// Экспорт сценариев
+export { svgSpriteTask };
+export { dev };
+export { build };
+export { deployZIP };
+export { deployFTP };
+
+// Выполнение сценария по умолчанию
+gulp.task("default", dev);
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
-console.log('ddawdawd');
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
