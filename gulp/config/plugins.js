0fee06fc8d5c39255ff4d9617ebb84dcf3874290
artem-mikhaylets <artem.mikhaylets@gmail.com>
2022-07-20 17:10:48 +0300

    end task 1

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,16 +1,13 @@
-import replace from "gulp-replace"; // Поиск и замена
-import plumber from "gulp-plumber"; // Обработка ошибок
-import notify from "gulp-notify"; // Сообщения (подсказки)
-import browsersync from "browser-sync"; // Локальный сервер
-import newer from "gulp-newer"; // Проверка обновления
-import ifPlugin from "gulp-if"; // Условное ветвление
+import replace from "gulp-replace";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import newer from "gulp-newer";
+import ifPlugin from "gulp-if";
 
-// Экспортируем объект
 export const plugins = {
 	replace: replace,
 	plumber: plumber,
 	notify: notify,
-	browsersync: browsersync,
 	newer: newer,
-	if: ifPlugin
-}
+	if: ifPlugin,
+};
