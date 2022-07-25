21524d74f606caa9962fd8eb2d035dc0cf8594e8
artem-mikhaylets <artem.mikhaylets@gmail.com>
2022-07-25 16:33:47 +0300

    create temp branch

--- README.md
+++
@@ -1,1 +0,0 @@
-Макет- https://www.figma.com/file/aOA6R4pp00AD69HTPfrvwq/%D0%9F%D0%94%D0%92-Landing-page?node-id=0%3A1
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+import { plugins } from "./gulp/config/plugins.js";
+global.app = {
+	isBuild: process.argv.includes("--build"),
+	isDev: !process.argv.includes("--build"),
+	path: path,
+	gulp: gulp,
+	plugins: plugins,
+};
+
+import { reset } from "./gulp/tasks/reset.js";
+import { scss } from "./gulp/tasks/styles-generate.js";
+import { js } from "./gulp/tasks/script-generate.js";
+function watcher() {
+	gulp.watch(path.watch.scss, scss);
+	gulp.watch(path.watch.js, js);
+}
+
+const mainTasks = gulp.series(gulp.parallel(scss, js));
+
+const dev = gulp.series(reset, mainTasks, watcher);
+const build = gulp.series(reset, mainTasks);
+
+export { dev };
+export { build };
+gulp.task("default", dev);
