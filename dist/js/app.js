fa7b91b26a232b681c526484df3209220dd94467
artem-mikhaylets <artem.mikhaylets@gmail.com>
2022-07-29 23:27:27 +0300

    create accardion menu

---
+++ dist/js/app.js
@@ -0,0 +1,22 @@
+var acc = document.getElementsByClassName("clients-dropdown__but");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "flex") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "flex";
+		}
+	});
+}
+let cdLink = document.querySelectorAll("clients-dropdown__link");
+for (i = 0; i < cdLink.length; i++) {
+	cdLink[i].addEventListener("click", function () {
+		cdLink.classList.toggle("clients-dropdown__link--active");
+	});
+}
+console.log("sanya fashist");
